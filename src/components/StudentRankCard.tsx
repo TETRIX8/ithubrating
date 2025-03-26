2d8711172bf4dda5ce3b4d2f5e1d44dbf930417c
AK project <132359965+TETRIX8@users.noreply.github.com>
2025-03-26 12:45:44 +0300

    Update StudentRankCard.tsx

--- src/components/StudentRankCard.tsx
+++ src/components/StudentRankCard.tsx
@@ -60,7 +60,7 @@ const StudentRankCard: React.FC<StudentRankProps> = ({
         </div>
         
         <Avatar className="h-10 w-10 border-2 border-primary/20">
-          <AvatarImage src={avatarUrl} alt={`${firstName} ${}`} />
+          <AvatarImage src={avatarUrl} alt={`${firstName} ${lastName}`} />
           <AvatarFallback className="bg-primary/10 text-primary">
             {initials}
           </AvatarFallback>
